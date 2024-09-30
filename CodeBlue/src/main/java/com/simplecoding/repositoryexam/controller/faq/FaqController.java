8c0527a4d167cb5ac80f19da2a3676a6b6188874
Snuckle110 <103734337+Snuckle110@users.noreply.github.com>
2024-09-30 13:53:55 +0900

    9.30 FAQ

--- CodeBlue/src/main/java/com/simplecoding/repositoryexam/controller/faq/FaqController.java
+++ CodeBlue/src/main/java/com/simplecoding/repositoryexam/controller/faq/FaqController.java
@@ -3,25 +3,40 @@
  */
 package com.simplecoding.repositoryexam.controller.faq;
 
+import java.util.List;
+
+import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.simplecoding.repositoryexam.service.basic.FaqService;
+import com.simplecoding.repositoryexam.vo.common.Criteria;
+
+import lombok.extern.log4j.Log4j;
 
 /**
  * @fileName : GameInfoController.java
  * @author : KTE
  * @since : 2024. 9. 20. description :
  */
+@Log4j
 @Controller
 public class FaqController {
+
+    @Autowired
+    FaqService faqService;
+
     @GetMapping("/faq")
     public String faqmain() {
 	return "faq/faqmain";
     }
 
-    @GetMapping("/faqlogin")
-    public String faqlogin() {
-	return "faq/faqlogin";
-    }
+    /*
+     * @GetMapping("/faqlogin") public String faqlogin() { return "faq/faqlogin"; }
+     */
 
     @GetMapping("/faqemail")
     public String faqemail() {
@@ -38,9 +53,31 @@ public class FaqController {
 	return "faq/faqbuy";
     }
 
-    @GetMapping("/faqright")
-    public String faqright() {
-	return "faq/faqright";
+    @GetMapping("/faqsearch")
+    public String faqsearch() {
+	return "faq/faqsearch";
+    }
+
+    @GetMapping("/faqlogin")
+    public String faqList(@ModelAttribute("searchVO") Criteria searchVO, Model model) throws Exception {
+
+	searchVO.setPageUnit(40);
+	searchVO.setPageSize(4);
+	PaginationInfo paginationInfo = new PaginationInfo();
+	paginationInfo.setCurrentPageNo(searchVO.getPageIndex());
+	paginationInfo.setRecordCountPerPage(searchVO.getPageUnit());
+	paginationInfo.setPageSize(searchVO.getPageSize());
+	searchVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+	searchVO.setLastIndex(paginationInfo.getLastRecordIndex());
+	searchVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+
+	List<?> faqs = faqService.selectFaqList(searchVO);
+	model.addAttribute("faqs", faqs);
+	int totcnt = faqService.selectFaqListTotCnt(searchVO);
+	paginationInfo.setTotalRecordCount(totcnt);
+	model.addAttribute("paginationInfo", paginationInfo);
+
+	return "faq/faqlogin"; // 해당 JSP 파일 이름
     }
 
 }
