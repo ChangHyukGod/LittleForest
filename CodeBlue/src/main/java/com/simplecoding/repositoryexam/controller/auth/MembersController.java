0a377e5abc87c58ef710d9274bf54bc38e54312f
wooseockJang <127363950+wooseockJang@users.noreply.github.com>
2025-01-04 19:05:53 +0900

    테스트용 커밋 x
    테스트용 커밋 x

--- CodeBlue/src/main/java/com/simplecoding/repositoryexam/controller/auth/MembersController.java
+++ CodeBlue/src/main/java/com/simplecoding/repositoryexam/controller/auth/MembersController.java
@@ -48,7 +48,7 @@ public class MembersController {
    public String loginView() {
       return "auth/login";
    }
-
+   // 테스트용 커밋 x
    // 회원가입 화면
    @GetMapping("/register")
    public String registerView() {
